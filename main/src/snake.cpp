dc05e1f7921e70d59b94b2d86cedf8b166a6f905
brighttarkang5-netizen <brighttarkang5@gmail.com>
2026-05-23 23:15:25 +0100

    Change main function to SDL_main for SDL compatibility

--- main/src/snake.cpp
+++ main/src/snake.cpp
@@ -1,6 +1,6 @@
 #include "SDL.h"
-
-int main(int argc, char *argv[])
+#include "SDL_main.h"
+int SDL_main(int argc, char *argv[])
 {
     // 1. Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
